a102a2ef4cfb379abf92605fc4196f922c4ea6b7
yarastouhi <yarastouhi12@hotmail.com>
2024-11-07 16:04:30 -0500

    screeningActivity xml

--- app/src/main/java/com/example/detectoma/ScreeningActivity.java
+++ app/src/main/java/com/example/detectoma/ScreeningActivity.java
@@ -3,6 +3,7 @@ package com.example.detectoma;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,47 +11,74 @@ import androidx.appcompat.widget.Toolbar;
 
 public class ScreeningActivity extends AppCompatActivity {
 
-    Button UploadImageButton;
+    private Button userDataButton, takePhotoButton, takeTempButton, takeDistButton, analyzeButton;
+    private CheckBox userDataCheckBox, takePhotoCheckBox, takeTempCheckBox, takeDistCheckBox;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_screening);
 
+        userDataButton = findViewById(R.id.userDataButton);
+        takePhotoButton = findViewById(R.id.takePhotoButton);
+        takeTempButton = findViewById(R.id.takeTempButton);
+        takeDistButton = findViewById(R.id.takeDistButton);
+        analyzeButton = findViewById(R.id.analyzeButton);
 
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        if (getSupportActionBar() != null) {
-            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-            getSupportActionBar().setDisplayShowHomeEnabled(true);
-            getSupportActionBar().setTitle("Screening");
-        }
+        userDataCheckBox = findViewById(R.id.userDataCheckBox);
+        takePhotoCheckBox = findViewById(R.id.takePhotoCheckBox);
+        takeTempCheckBox = findViewById(R.id.takeTempCheckBox);
+        takeDistCheckBox = findViewById(R.id.takeDistCheckBox);
 
-        UploadImageButton = findViewById(R.id.uploadImageButton);
-        UploadImageButton.setOnClickListener(v -> {
-            Intent intent = new Intent(ScreeningActivity.this, ScreeningMlActivity.class);
-            startActivity(intent);
-        });
+        userDataButton.setOnClickListener(v -> openUserDataDialog());
+        takePhotoButton.setOnClickListener(v -> openTakePhotoActivity());
+        takeTempButton.setOnClickListener(v -> openTakeTempDialog());
+        takeDistButton.setOnClickListener(v -> openTakeDietDialog());
 
-        TextView screeningTextView = findViewById(R.id.screeningTextView);
-        screeningTextView.setText("Please upload pictures from the device");
+        analyzeButton.setOnClickListener(v -> openResultsActivity());
+
+        updateButtonState();
     }
 
-    @Override
-    public boolean onSupportNavigateUp() {
-        navigateToHome();
-        return true;
+    private void updateButtonState() {
+        takePhotoButton.setEnabled(userDataCheckBox.isChecked());
+        takeTempButton.setEnabled(takePhotoCheckBox.isChecked());
+        takeDistButton.setEnabled(takeTempCheckBox.isChecked());
+        analyzeButton.setEnabled(userDataCheckBox.isChecked() && takePhotoCheckBox.isChecked() &&
+                takeTempCheckBox.isChecked() && takeDistCheckBox.isChecked());
     }
 
-    @Override
-    public void onBackPressed() {
-        super.onBackPressed();
-        navigateToHome();
+    private void openUserDataDialog() {
+        // Open a DialogFragment to gather user data
+        // Once completed:
+        userDataCheckBox.setChecked(true);
+        updateButtonState();
     }
 
-    private void navigateToHome() {
-        Intent intent = new Intent(ScreeningActivity.this, HomeActivity.class);
+    private void openTakePhotoActivity() {
+        // Open an Activity to take a photo
+        // Once completed:
+        takePhotoCheckBox.setChecked(true);
+        updateButtonState();
+    }
+
+    private void openTakeTempDialog() {
+        // Open a DialogFragment to take temperature
+        // Once completed:
+        takeTempCheckBox.setChecked(true);
+        updateButtonState();
+    }
+
+    private void openTakeDietDialog() {
+        // Open a DialogFragment to enter diet information
+        // Once completed:
+        takeDistCheckBox.setChecked(true);
+        updateButtonState();
+    }
+
+    private void openResultsActivity() {
+        // Open ResultsActivity once all items are completed
+        Intent intent = new Intent(ScreeningActivity.this, resultsActivity.class);
         startActivity(intent);
-        finish();
     }
 }
